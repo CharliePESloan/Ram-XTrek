f83ca2b5924a8fe05be6f76132855e3975aacfd4
FishToastie <CharliePESloan@gmail.com>
2018-02-16 12:52:08 +0000

    Added HttpConnect.java from workshops

--- Directions.java
+++ Directions.java
@@ -4,13 +4,15 @@ public class Directions
 {
 	/* Constant Values */
 	final static String URLBASE = "https://maps.googleapis.com/maps/api/directions/json";
-	final static String method = "GET";
+	final static String METHOD  = "GET";
+	final static String REGION  = "uk";
+	final static String MODE    = "walking";
 
 	/* Navigation Variables */
-	private      String origin;
-	private      String destination;
-	private      String region;
-	private      String mode;
+	private	String origin;
+	private	String destination;
+	private	byte[] rawDirections = {};
+	//private      String region;
 
 	/*
 	 * Constructor
@@ -36,7 +38,7 @@ public class Directions
 		destination = String.valueOf(latitude)+","+String.valueOf(longitude);
 	}
 
-	static void refreshDirections()
+	public void refreshDirections()
 	{
 		try
 		{
@@ -46,15 +48,34 @@ public class Directions
 			final String url = (URLBASE
 					   +"?origin="	   +encOrigin
 					   +"&destination="+encDestination
-					   +"&region="	   +region)
+					   +"&region="	   +REGION
+					   +"&mode="	   +MODE);
+			final byte[] body = {};
+			final String[][] headers = {};
+			byte[] rawDirections = HttpConnect.httpConnect( METHOD, url, headers, body );
 		} catch (Exception ex)
 		{
 			System.out.println( ex ); System.exit( 1 );
+		}
 	}
 
-	public void PrintOut()
+	public void printOut()
 	{
 		System.out.println("Origin="+origin);
 		System.out.println("Destination="+destination);
+		System.out.println("Directions:");
+		for (int i=0; i < rawDirections.length; i++)
+		{
+			System.out.print( (char) rawDirections[i]);
+		}
+	}
+
+	public static void main(String args[])
+	{
+		Directions myDir = new Directions();
+		myDir.setOrigin	(50.7236f, -3.52751f);
+		myDir.setDest	(51.3758f,  2.3599f);
+		myDir.refreshDirections();
+		myDir.printOut();
 	}
 }
