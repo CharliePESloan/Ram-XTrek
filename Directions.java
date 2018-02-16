673ade80cfa74ad5d687fe99c262001c6843bcbe
FishToastie <CharliePESloan@gmail.com>
2018-02-16 11:44:33 +0000

    Testing git, minor changes to Directions.java

--- Directions.java
+++ Directions.java
@@ -1,5 +1,12 @@
+import java.net.URLEncoder;
+
 public class Directions
 {
+	private String ORIGIN;
+	private String DESTINATION;
+	private String REGION;
+	private String MODE;
+
 	public Directions()
 	{}
 }
