906a034a42c41cf71a3a9c031a396eb5ef860a6c
devashpatel <devashpatel@users.noreply.github.com>
2018-02-21 14:34:38 +0000

    changes to maps

--- ModelViewController.java
+++ ModelViewController.java
@@ -13,17 +13,17 @@ import javax.swing.ImageIcon;
  */
 public class ModelViewController{
 	
-	
-
-  
   public static void main( String[] argv ) {
     Model      model      = new Model(0);
     Controller controller = new Controller( model );
     View       view       = new View( controller, model );
 
     JFrame frame = new JFrame();
+	frame.add(view); 
+	frame.setVisible(true);
     frame.add( view );
-    frame.setVisible( true );
-	frame.setSize(1200,1200); 
+	//frame.add(XTrex);
+	//frame.setContentPane( new JLabel( new ImageIcon( "XTrex Background.jpep" ) ) );
+	//frame.setSize(1200,1200); 
   }
 }
