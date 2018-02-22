7a2798e7744e0066842d44a8f6551627c1fa0cb0
Gigandeth <36537092+Gigandeth@users.noreply.github.com>
2018-02-22 22:11:35 +0000

    Commented code
    Commented ;)

--- SatelliteFrame.java
+++ SatelliteFrame.java
@@ -17,14 +17,20 @@ import java.awt.image.BufferedImage;
 import java.awt.*;
 import javax.swing.*;
 
+/*Work Package 5*/
+
 public class SatelliteFrame extends BlankXTrex {
-    
+	/*XTrex button used for navigation, final as they are constant and won't be changed*/
     final SideButton    PlusButton    = new BlankXTrex.SideButton("PlusButton");
     final SideButton    MinusButton   = new BlankXTrex.SideButton("MinusButton");
     final SideButton    SelectButton  = new BlankXTrex.SideButton("SelectButton");
     final SideButton    MenuButton    = new BlankXTrex.SideButton("MenuButton");
     
     public SatelliteFrame(){
+		/*This code here has been provided by my team mate in order for me to integrate my 
+		 *package with the GUI to display the result of my functions.
+		 *They set the up the XTrex look and take attributes and properties from the 
+		 *BlankXTrex who is a parent to this class, for me to then add to the work*/
         setTitle( "XTrex" );
         JLabel content = new JLabel(new ImageIcon("XTrex Background.png"));
         //content.add("XTrex Background.png");
@@ -38,31 +44,41 @@ public class SatelliteFrame extends BlankXTrex {
         setSize( 450, 835 ); /* title bar! */ 
         setResizable( false );
         
-        /*Getting data from the Satellite Dongle*/
+        //Making a satellite object
 		Satellite mySat = new Satellite();
 		// Using dummy values from a dongle reading
-		String a[] = mySat.getGLL("$GPGLL,5043.94335,N,00330.91606,W,111950.00,A,A*74");
+		String a[] = mySat.getGLL("$GPGLL,5043.94335,N,00330.91606,W,111950.00,A,A*74"); // dummy value
 		
-        JLabel longitude = new JLabel(a[0] + " " + a[1], SwingConstants.CENTER);
+		//Inserting labels into our frame which are going to contain our position data
+        JLabel longitude = new JLabel(a[0] + " " + a[1], SwingConstants.CENTER); 
+		//The values used are those stored and returned from our array
 		JLabel latitude = new JLabel(a[2] + " " + a[3], SwingConstants.CENTER );
 		
+		//Position our label in the page, put them next to each other
 		longitude.setBounds(120, 350, 200, 80);
 		latitude.setBounds(120, 425, 200, 80);
 		
-		longitude.setOpaque(true);
-		latitude.setOpaque(true);
+		//The following lines contain information about coloring for label personalisation
+		//Will included them into my next sprint, commented out as I am not currently using them
+		
+		/*longitude.setOpaque(true);
+		latitude.setOpaque(true);*/
 		
 		/*longitude.setBackground(Color.WHITE);
 		longitude.setBackground(Color WHITE);*/
 		
-		add(longitude);
+		add(longitude); //labels added to frame
 		add(latitude);
-		     
+		
+		//Buttons instantiated above are set in position on the frame and added to it
         PlusButton.setBounds(9, 102, 30, 68);add(PlusButton);
         MinusButton.setBounds(11, 175, 27, 64);add(MinusButton);
         SelectButton.setBounds(5, 268, 34, 97); add(SelectButton);
         MenuButton.setBounds(409, 113, 30,84); add(MenuButton);
         
+		/*This button allows me to move from this page to the Main Menu,
+		 *it does so by creating a new instance when clicked. The same procedure
+		 *will be used to redirect the user from the main menu to any page*/
         MenuButton.addMouseListener(new java.awt.event.MouseAdapter(){
             public void mouseClicked(java.awt.event.MouseEvent evt){
                 dispose();
@@ -77,7 +93,8 @@ public class SatelliteFrame extends BlankXTrex {
         
     
   
-  
+	/*Making an instance of my public class in order to display on the screen
+	 *for user interaction. A frame with its components is created*/
 	public static void main(String[] args){
 		JFrame frame = new SatelliteFrame();
 		frame.setLocationRelativeTo(null);
@@ -88,22 +105,17 @@ public class SatelliteFrame extends BlankXTrex {
 }
 
 class Satellite{
-	
-	/*public static void main(String[] args){
-		Satellite mySat = new Satellite();
-		String[] a = mySat.getGLL("$GPGLL,5043.94335,N,00330.91606,W,111950.00,A,A*74\n$GPGLL,5043.94335,N,00330.91606,W,111950.00,A,A*74\n");
-		for(int i = 0; i < a.length; i++){
-		    System.out.println(a[i]);
-		}
-	}*/
-	
+
+	/*Constructor which enables access to methods defined within this class*/
 	public Satellite(){};
 	
+	/*This method return appends the GLL sentence to an array
+	which allows us to store the part of the sentence for further purposes*/
 	public String[] getPosition(String coordinate){
 	    String[] components;
 		if(coordinate.startsWith("$GPGLL")){
-			String message = coordinate.substring(7);
-			components = message.split(",");
+			String message = coordinate.substring(7); // We ignore the characters which aren't required 
+			components = message.split(","); // remove all the commas to get the words
 		    return components;
 		}
 		else{
@@ -111,6 +123,10 @@ class Satellite{
 		}
 	}
 	
+	/*Splits the various GPS messages and stores them inside an array,
+	 *this array is then scanned by using the getPosition method to get only
+	 *the GLL sentences. At the moments it only returns the first value as we have been using
+	 *dummy values due to issues with the dongle. Further sprints will contain an iteration through the array*/
 	public String[] getGLL(String text){
 		String [] lines = text.split("\n");
 		return getPosition(lines[0]);
