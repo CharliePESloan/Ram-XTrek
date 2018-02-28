2789862a456a50cd0c745b7ce8def17f3f82ccb7
FishToastie <CharliePESloan@gmail.com>
2018-02-28 16:51:32 +0000

    Updated controller

--- Controller.java
+++ Controller.java
@@ -4,34 +4,54 @@ import javax.swing.SwingUtilities;
 /*
  * Controller.
  *
- * David Wakeling 2018.
+ * By Charlie Sloan and David Wakeling (2018)
  */
 class Controller implements MouseListener{
-  private Model model;
+	private Model model;
 
-  public Controller( Model model ) {	
-    this.model = model;
-  }
+	public Controller( Model model ) {	
+		this.model = model;
+	}
 
-  public void mouseClicked( MouseEvent me) {
-	if (SwingUtilities.isLeftMouseButton(me))
+	public void switchModel( Model model )
+	{
+		this.model = model;
+	}
+
+	public void mouseClicked( MouseEvent me) {
+		/*if (SwingUtilities.isLeftMouseButton(me))
+		{
+			model.zoomOut(); 
+		}
+		else if (SwingUtilities.isRightMouseButton(me))
+		{
+			model.zoomIn();
+		}*/
+		switch (me.button.getText())
+		{
+			case "PlusButton":
+				this.model.pressedPlus();
+				break;
+			case "MinusButton":
+				this.model.pressedMinus();
+				break;
+			case "SelectButton":
+				this.model.pressedSelect();
+				break;
+			case "MenuButton":
+				this.model.pressedPlus();
+				break;
+		}
+	}
+
+
+	public void mousePressed (MouseEvent me)
 	{
 		model.zoomOut(); 
 	}
-	else if (SwingUtilities.isRightMouseButton(me))
-	{
-		model.zoomIn();
-	}
-  }
-  
-  
-  
-  public void mousePressed (MouseEvent me) {
-	model.zoomOut(); 
-  }
-  
 
-  public void mouseEntered ( MouseEvent me ) { /* nothing */ }
-  public void mouseExited  ( MouseEvent me ) { /* nothing */ }
-  public void mouseReleased( MouseEvent me ) { /* nothing */ }
+
+	public void mouseEntered ( MouseEvent me ) { /* nothing */ }
+	public void mouseExited  ( MouseEvent me ) { /* nothing */ }
+	public void mouseReleased( MouseEvent me ) { /* nothing */ }
 }
