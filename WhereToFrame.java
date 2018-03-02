289ab42f3198b6086c3c6ca8c85509543ccd9923
nathan727 <32885382+nathan727@users.noreply.github.com>
2018-03-02 15:58:14 +0000

    update

--- WhereToFrame.java
+++ WhereToFrame.java
@@ -1,18 +1,8 @@
-import java.awt.Color;
-import java.awt.TextField;
+import java.io.*;
+import java.net.URL;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.ImageIcon;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.URL;
-import java.awt.Toolkit;
-import java.awt.Dimension;
-import java.awt.*;
 import javax.swing.*;
 
 public class WhereToFrame extends BlankXTrex {
@@ -93,7 +83,7 @@ public class WhereToFrame extends BlankXTrex {
 
         MenuButton.addMouseListener(new java.awt.event.MouseAdapter(){
             public void mouseClicked(java.awt.event.MouseEvent evt){
-                dispose();
+                //dispose();
                 new XTrex();
             }
         });
@@ -172,7 +162,7 @@ public class WhereToFrame extends BlankXTrex {
 			    display.setText("" + textDisplay);
 			}
                         if (selected == buttonRight) {
-                            dispose();
+                            //dispose();
                             new WhereToFrameTwo();
                         }
 			else {
@@ -181,4 +171,99 @@ public class WhereToFrame extends BlankXTrex {
                         }
            }
 });
-}}
+}
+public class WhereToFrameTwo extends BlankXTrex {
+    
+    final TextField display = new TextField();
+    TextButton[] numberButtons = new TextButton[10];
+    ExtractButton selected;
+    
+    final TextButton buttonLeft = new TextButton("LEFT");
+    final TextButton buttonDel = new TextButton("DEL");
+    
+    
+    public String removeChar(String s, int a) {
+        return s.substring(0, a) + s.substring(a + 1);
+    }    
+    public WhereToFrameTwo() {
+        super();
+        
+        for (int i=0; i<10;++i){
+            numberButtons[i] = new TextButton(Integer.toString(i));
+            add(numberButtons[i]);
+        }
+        PlusButton.setBounds(9, 102, 30, 68);add(PlusButton);
+        MinusButton.setBounds(11, 175, 27, 64);add(MinusButton);
+        SelectButton.setBounds(5, 268, 34, 97); add(SelectButton);
+        MenuButton.setBounds(409, 113, 30,84); add(MenuButton);
+        display.setBounds (95, 312, 260, 50); add(display);
+        
+        for (int i=2; i<9;i++) {
+			numberButtons[i].setPrevNext(numberButtons[i-1],numberButtons[i+1]);
+		}
+        numberButtons[0].setPrevNext(numberButtons[9], buttonLeft);
+        buttonLeft.setPrevNext(numberButtons[0], buttonDel);
+        numberButtons[1].setPrevNext(buttonDel, numberButtons[2]);
+        buttonDel.setPrevNext(buttonLeft, numberButtons[1]);
+        numberButtons[9].setPrevNext(numberButtons[8], numberButtons[0]);
+        
+        selected = (ExtractButton) numberButtons[1];
+        numberButtons[1].select();
+                                
+        numberButtons[1].setBounds (95, 365, 86, 63);
+        numberButtons[2].setBounds (181, 365, 86, 63);
+        numberButtons[3].setBounds (267, 365, 86, 63);
+        
+        numberButtons[4].setBounds (95, 428, 86, 63);
+        numberButtons[5].setBounds (181, 428, 86, 63);
+        numberButtons[6].setBounds (267, 428, 86, 63);
+        
+        numberButtons[7].setBounds (95, 491, 86, 63);
+        numberButtons[8].setBounds (181, 491, 86, 63);
+        numberButtons[9].setBounds (267, 491, 86, 63);
+        
+        numberButtons[0].setBounds (95, 554, 86, 63);
+        buttonLeft.setBounds(95, 617, 86, 63); add(buttonLeft);
+        buttonDel.setBounds(181, 554, 172, 126); add(buttonDel);
+        
+        
+        MenuButton.addMouseListener(new java.awt.event.MouseAdapter(){
+            public void mouseClicked(java.awt.event.MouseEvent evt){
+                dispose();
+                new XTrex();
+            }
+        });
+        
+        PlusButton.addMouseListener(new MouseAdapter() {
+        public void mouseClicked( MouseEvent me) {
+			selected = selected.next();
+		}
+    });
+
+        MinusButton.addMouseListener( new MouseAdapter() {
+            public void mouseClicked(MouseEvent evt) {
+                selected = selected.prev();
+            }
+        });
+        SelectButton.addMouseListener(new MouseAdapter(){
+           public void mouseClicked(MouseEvent me) {
+                        if (selected == buttonLeft) {
+                            dispose();
+                            new WhereToFrame();
+                        }
+                        if (selected == buttonDel) {
+                            if (textDisplay.length() != 0){
+                                textDisplay = removeChar(textDisplay, textDisplay.length()-1);
+                                display.setText("" + textDisplay);
+                            }
+                        }
+			else {
+                            textDisplay += selected.getText();
+                            display.setText("" + textDisplay);
+                        }
+           }
+});
+    }
+}
+
+}
