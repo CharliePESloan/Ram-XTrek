3eae9b0244015882e64babaf4524e3b23295164e
FishToastie <CharliePESloan@gmail.com>
2018-03-08 17:27:45 +0000

    Improved bat file

--- jcr.bat
+++ jcr.bat
@@ -1,3 +1,3 @@
 if not exist "bin" mkdir bin
-javac -d bin -classpath ".;jsoup-1.11.2.jar;json-20180130.jar;RXTXcomm.jar" $1.java
-java  -cp "bin;jsoup-1.11.2.jar;json-20180130.jar;RXTXcomm.jar" $1
+javac -d bin -classpath ".;jsoup-1.11.2.jar;json-20180130.jar;RXTXcomm.jar" %1.java
+java  -cp "bin;jsoup-1.11.2.jar;json-20180130.jar;RXTXcomm.jar" %1
