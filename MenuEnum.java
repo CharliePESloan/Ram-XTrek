ea64e8bfa00e1b535f1f949c4c2c4e4bf2175fe5
FishToastie <CharliePESloan@gmail.com>
2018-03-16 16:18:59 +0000

    Changed trip computer enum

--- MenuEnum.java
+++ MenuEnum.java
@@ -4,14 +4,14 @@ public enum MenuEnum {
     @Override 
     public String toString() {
     switch(this) {
-      case MAPS: return "Maps";
-      case WHERETO: return "WhereTo";
-      case TRIPCOMP: return "TripComp";
-      case MENU: return "Menu";
-      case SPEECH: return "Speech";
-      case ABOUT: return "About";
-      case SATELLITE: return "Satellite";
-      case ONOFF: return "OnOff";
+      case MAPS:	return "Maps";
+      case WHERETO:	return "WhereTo";
+      case TRIP:	return "TripComp";
+      case MENU:	return "Menu";
+      case SPEECH:	return "Speech";
+      case ABOUT:	return "About";
+      case SATELLITE:	return "Satellite";
+      case ONOFF:	return "OnOff";
       default: throw new IllegalArgumentException();
     }
   }
