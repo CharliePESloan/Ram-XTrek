cad430a2f7a049455c74e95bb6196ece00d21867
FishToastie <CharliePESloan@gmail.com>
2018-03-21 17:02:35 +0000

    Fixed two bugs

--- MenuFrame.java
+++ MenuFrame.java
@@ -61,7 +61,7 @@ public class MenuFrame extends JFrame {
     JButton MenuButton = new JButton();
     JButton OnOffButton = new JButton();
 
-    Navigator nav = new Navigator(speechModel,satModel,whereToModel);
+    Navigator nav = new Navigator(this,speechModel,satModel,whereToModel);
      Language language = new Language("en");
     Speaker speaker = new Speaker(language, speechModel);
     String currentView = "OnOff";
